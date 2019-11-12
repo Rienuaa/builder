a981eb176af80dd537bea428c300ce628e645dbf
Q <aaacantor@gmail.com>
2019-11-12 14:30:15 -0500

    php tests

---
+++ result.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="materialz-master/material-elements.css" />
+		<link rel="stylesheet" type="text/css" href="style.css" />
+		<title>Clean Builder</title>
+	</head>
+	<body>
+		<b><font size = "6">A Clean Build</font></b>
+		<p>A potentially open-source offline larp character building platform. @Rienuaa on twitter and github. This is a glorified php form with some light hackery. VERY WIP</p>
+		
+		Player Name: <?php echo $_POST["playername"]; ?><br>
+		Header: <?php echo $_POST["header"]; ?>
+		
+		
+<script type="text/javascript" src="builder.js"></script></body></html>
