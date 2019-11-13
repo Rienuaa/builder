905944c6fe4e6c2255fcf72b501d6f43d1d657e3
Q <aaacantor@gmail.com>
2019-11-12 20:04:09 -0500

    more nonsense
    fuck php

--- builder.js
+++ builder.js
@@ -1,5 +1,16 @@
-var name = "";
+var playername = "";
 var build = 0;
 var header = "";
 var culture = "";
 
+function display() {
+	//stores relevant info
+	playername = document.getElementById("playername").value;
+	header = document.getElementById("header").value;
+	culture = document.getElementById("culture").value;
+	
+	//makes things pretty
+    document.getElementById('playernameDisplay').innerHTML = playername;
+    document.getElementById('headerDisplay').innerHTML = header;
+    document.getElementById('cultureDisplay').innerHTML = culture;
+}
